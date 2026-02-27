21de1550740f2d4903aa31a614da40abe3552f5c
AzzieDev <18339356+AzzieDev@users.noreply.github.com>
2026-02-26 23:36:23 -0500

    docs: remove plain text email from swagger info block to prevent bot scraping

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,18 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="Time Since API",
+ *      description="L5 Swagger OpenApi description for the Time Since application"
+ * )
+ *
+ * @OA\Server(
+ *      url=L5_SWAGGER_CONST_HOST,
+ *      description="API Server"
+ * )
+ */
 abstract class Controller
 {
     //
